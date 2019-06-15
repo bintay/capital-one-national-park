39c61627007fe8f214018584ab137e7cd584f765
Ben Taylor <bentaylor@bentaylor.me>
2019-06-15 17:04:11 -0500

    fix: slightly more responsive

--- client/src/components/results.js
+++ client/src/components/results.js
@@ -3,15 +3,15 @@ import campingImage from "../images/camping.svg";
 import Typography from "@material-ui/core/Typography"
 
 const Results = (props) => (
-   <div style={{ maxWidth: "60%", margin: "60px auto 0 auto", maxHeight: "90vh" }}>
+   <div style={{ maxWidth: "50%", margin: "60px auto 0 auto", maxHeight: "90vh" }}>
       {
          props.parks.length > 0
          ?
             <p>Parks go here</p>
          :
             <div>
-               <img src={campingImage} alt="Search above for parks" style={{ maxHeight: "90vh", opacity: 0.9 }} />
-               <Typography variant="h4" component="h2" style={{ textAlign: "center", position: "relative", top: -120, color: "#666", fontWeight: 300, marginBottom: -120 }} >
+               <img src={campingImage} alt="Search above for parks" style={{ opacity: 0.9 }} />
+               <Typography variant="h4" component="h2" style={{ textAlign: "center", position: "relative", top: '-7vw', color: "#666", fontWeight: 300, marginBottom: '-7vw' }} >
                  Search for Parks Above
                </Typography>
             </div>
