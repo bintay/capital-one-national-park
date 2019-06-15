ed9495dcba09dc25734a2b6e82cdbe9b1c5b0ea3
Ben Taylor <bentaylor@bentaylor.me>
2019-06-15 17:50:17 -0500

    feat: lists parks

---
+++ client/src/components/park-card.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import Card from '@material-ui/core/Card';
+import CardActionArea from '@material-ui/core/CardActionArea';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import CardMedia from '@material-ui/core/CardMedia';
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+
+const ParkCard = (props) => (
+  <Card style={{ width: "80%", minWidth: 300, margin: "0 auto 20px auto" }}>
+    <CardActionArea>
+      {
+        props.park.images && props.park.images.length > 0
+          ? <CardMedia
+              style={{ height: 280 }}
+              image={props.park.images[0].url}
+              title={props.park.images[0].title}
+            />
+          : null
+      }
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="h2">
+          {props.park.name}
+        </Typography>
+        <Typography variant="body2" color="textSecondary" component="p">
+          {props.park.description}
+        </Typography>
+      </CardContent>
+    </CardActionArea>
+    <CardActions>
+      <Button size="small" color="primary">
+        Learn More
+      </Button>
+      {
+        props.park.url 
+          ? <Button size="small" color="primary" href={props.park.url}>
+              Official Website
+            </Button>
+          : null
+      }
+    </CardActions>
+  </Card>
+)
+
+export default ParkCard;
--- client/src/components/results.js
+++ client/src/components/results.js
@@ -1,22 +1,29 @@
 import React from "react";
 import campingImage from "../images/camping.svg";
-import Typography from "@material-ui/core/Typography"
+import Typography from "@material-ui/core/Typography";
+import ParkCard from "./park-card";
 
 const Results = (props) => (
-   <div style={{ maxWidth: "50%", margin: "60px auto 0 auto", maxHeight: "90vh" }}>
-      {
-         props.parks.length > 0
-         ?
-            <p>Parks go here</p>
-         :
-            <div>
-               <img src={campingImage} alt="Search above for parks" style={{ opacity: 0.9 }} />
-               <Typography variant="h4" component="h2" style={{ textAlign: "center", position: "relative", top: '-7vw', color: "#666", fontWeight: 300, marginBottom: '-7vw' }} >
-                 Search for Parks Above
-               </Typography>
-            </div>
-      }
-   </div>
+  <div style={{ maxWidth: "50%", margin: "60px auto 0 auto", maxHeight: "90vh" }}>
+    {
+      props.parks && props.parks.length > 0
+      ?
+        <div style={{ marginTop: 120 }}>
+          {
+            props.parks.map(park => (
+              <ParkCard park={park} />
+            ))
+          }
+        </div>
+      :
+        <div>
+          <img src={campingImage} alt="Search above for parks" style={{ opacity: 0.9 }} />
+          <Typography variant="h4" component="h2" style={{ textAlign: "center", position: "relative", top: '-7vw', color: "#666", fontWeight: 300, marginBottom: '-7vw' }} >
+            Search for Parks Above
+          </Typography>
+        </div>
+    }
+  </div>
 )
 
 export default Results;
