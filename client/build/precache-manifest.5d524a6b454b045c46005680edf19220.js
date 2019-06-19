8e7d10baabb520d94e4dc5e456937452dc0ffb0a
Ben Taylor <bentaylor@bentaylor.me>
2019-06-19 00:25:55 -0500

    include build b/c react wont't install on digital ocean

---
+++ client/build/precache-manifest.5d524a6b454b045c46005680edf19220.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28c79dfea74a8bc7e3e2810fd851be51",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51234d4d831670de5573",
+    "url": "/static/css/main.646d7ce6.chunk.css"
+  },
+  {
+    "revision": "440a1999b71f91a3a688",
+    "url": "/static/js/2.a7d5fd51.chunk.js"
+  },
+  {
+    "revision": "51234d4d831670de5573",
+    "url": "/static/js/main.bae52ac3.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5a4ab9750825036291bff33c667ef848",
+    "url": "/static/media/camping.5a4ab975.svg"
+  }
+]);
